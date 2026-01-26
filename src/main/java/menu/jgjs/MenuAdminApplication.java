30d77d3ed2c8c5be6a9af878bafc96795988f281
jgjs <jgjs@uniess.co.kr>
2026-01-26 15:03:36 +0900

    feat: DB 연동 및 이름 변경

--- src/main/java/menu/jgjs/MenuAdminApplication.java
+++ src/main/java/menu/jgjs/MenuAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BatchApplication {
+public class MenuAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchApplication.class, args);
+		SpringApplication.run(MenuAdminApplication.class, args);
 	}
 
 }
